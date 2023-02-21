c7830e11a6934d56b5eb7ce85b7f27683f7e1307
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2023-02-21 19:38:24 +0300

    Create constants

---
+++ app/src/main/java/com/yagmurerdogan/satellite/util/Constant.kt
@@ -0,0 +1,10 @@
+package com.yagmurerdogan.satellite.util
+
+object Constant {
+    const val IO_ERROR = "IO_ERROR"
+    const val LIST_EMPTY_ERROR = "LIST_EMPTY_ERROR"
+    const val SATELLITE_FILE = "satellite-list.json"
+    const val SATELLITE_DETAIL_FILE = "satellite-detail.json"
+    const val SATELLITE_POSITION_FILE = "positions.json"
+    const val POSITION_CHANGE_DELAY = 3000L
+}
