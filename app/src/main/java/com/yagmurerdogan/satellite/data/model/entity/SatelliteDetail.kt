e942fcadcb54603db6cbd1383cf2750e857bd1e9
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2023-02-21 23:54:52 +0300

    Add serialized name to entity class

--- app/src/main/java/com/yagmurerdogan/satellite/data/model/entity/SatelliteDetail.kt
+++ app/src/main/java/com/yagmurerdogan/satellite/data/model/entity/SatelliteDetail.kt
@@ -2,13 +2,16 @@ package com.yagmurerdogan.satellite.data.model.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "satellite_detail")
 data class SatelliteDetail(
     @PrimaryKey(autoGenerate = true)
     val id : Int,
     val satelliteId: Int,
+    @SerializedName("cost_per_launch")
     val costPerLaunch: Long,
+    @SerializedName("first_flight")
     val firstFlight: String,
     val height: Int,
     val mass: Long
