84228681a1d8d1a5d235649aaaab209577b00101
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2023-02-21 20:01:32 +0300

    Create mappers for ui models

---
+++ app/src/main/java/com/yagmurerdogan/satellite/domain/mapper/SatelliteDetailUIModelMapper.kt
@@ -0,0 +1,28 @@
+package com.yagmurerdogan.satellite.domain.mapper
+
+import com.yagmurerdogan.satellite.data.model.entity.SatelliteDetail
+import com.yagmurerdogan.satellite.data.model.entity.SatellitePosition
+import com.yagmurerdogan.satellite.domain.model.SatelliteDetailUIModel
+import java.text.NumberFormat
+
+object SatelliteDetailUIModelMapper {
+
+    fun satelliteDetailToUIModel(
+        list: List<SatelliteDetail?>?,
+        listPosition: List<SatellitePosition?>?
+    ): List<SatelliteDetailUIModel?>? {
+        return list?.map { satelliteDetail ->
+            SatelliteDetailUIModel(
+                satelliteId = satelliteDetail?.satelliteId,
+                dateText = satelliteDetail?.firstFlight,
+                heightMassText = satelliteDetail?.height.toString().plus("/")
+                    .plus(
+                        NumberFormat.getNumberInstance().format(satelliteDetail?.mass).toString()
+                    ),
+                costText = (NumberFormat.getNumberInstance()
+                    .format(satelliteDetail?.costPerLaunch)).toString(),
+                lastPosition = listPosition?.filter { position -> satelliteDetail?.satelliteId == position?.satelliteId }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/yagmurerdogan/satellite/domain/mapper/SatelliteUIModelMapper.kt
@@ -0,0 +1,20 @@
+package com.yagmurerdogan.satellite.domain.mapper
+
+import com.yagmurerdogan.satellite.R
+import com.yagmurerdogan.satellite.data.model.response.Satellite
+import com.yagmurerdogan.satellite.domain.model.SatelliteUIModel
+
+
+object SatelliteUIModelMapper {
+
+    fun satelliteFileResponseToUIModel(list: List<Satellite?>?): List<SatelliteUIModel?>? {
+        return list?.map {
+            SatelliteUIModel(
+                satelliteId = it?.satelliteId,
+                name = it?.name,
+                activeText = if (it?.active == true) "Active" else "Passive",
+                activeImg = if (it?.active == true) (R.drawable.ic_green_circle) else (R.drawable.ic_red_circle)
+            )
+        }
+    }
+}
